33a192f53ebfe87877ae1ce0cb2b22bf05dc7bb5
github username <github email address>
2020-06-02 08:03:26 -0500

    fixed delete bug

--- journal-frontend/src/components/entries.js
+++ journal-frontend/src/components/entries.js
@@ -8,7 +8,7 @@ class Entries {
         this.initEventListeners()
         this.fetchAndRenderEntries()
         
-        // this.likes = []
+       
        
     }
 
@@ -124,19 +124,19 @@ class Entries {
 
 
             fetch(`http://localhost:3000/entries/${id}/`, configObj)
-            
+        
             .then(resp => resp.json())
-    
-            }
+
+        
             if (e.target.className = "button.likes"){
                 
                 let count = e.target.parentElement.querySelector('.count')
                 count.innerHTML = (parseInt(count.innerText) + 1)
             }
-                
+            // render json: { likes_count: @entry.likes.length}
         }
-    // }
-        // # render json: { likes_count: @entry.likes.length}
+    }
+        
        
     
     
@@ -168,27 +168,10 @@ class Entries {
         })
         
     }
-
-    myFunction() {
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById('myInput');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("myUL");
-        li = ul.getElementsByTagName('li');         
-            // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("a")[0];
-            txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-
-        }
-
-        }
-    }
 }
+        
+
+// 
 
     
 
